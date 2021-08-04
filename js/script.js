7cdc3aa5c79cf7baabc91d82fc1368169d2b629a
wrang308 <markus.wrang_94@hotmail.com>
2021-08-04 08:31:19 +0200

    added comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-
-const API = "https://noroff-komputer-store-api.herokuapp.com/";
+/**
+ * Adding all element that is being used for Komputer Store
+ */
 const computersElement = document.getElementById("computersElement");
 const computerFeatures = document.getElementById("computerFeatures");
 const computerImage = document.getElementById("computerImage");
@@ -16,36 +17,56 @@ const payElement = document.getElementById("pay");
 const buyButton = document.getElementById("buy");
 const loanContainer = document.getElementById("loanContainer");
 
+const API = "https://noroff-komputer-store-api.herokuapp.com/";
 let computers = [];
 let balance = 0;
 let loan = 0;
 let pay = 0;
 
+/**
+ * Makes a request to the API to fetch the JSON object and puts into the computer array
+ */
 fetch(API+"computers")
     .then(response => response.json())
     .then(data => computers = data )
-    .then(computers => addComputersDropDown(computers));
-
-const addComputersDropDown = (computers) => {
-    computers.forEach(element => addComputersToDropDown(element));
+    .then(computers => addComputersToDropDown(computers))
+    .catch((error) => {
+        console.log(error)
+    });
+/**
+ * Adds all elements from the computer to the dropdown menu. After it is done it renders the info about the first computer
+ * @param {*} computers An array of computers
+ */
+const addComputersToDropDown = (computers) => {
+    computers.forEach(element => addComputersToDropDownMenu(element));
     
     //First render
     renderComputer(computers[0]);
 }
-
-const addComputersToDropDown = (computer) => {
+/**
+ * Takes a computer object and adds it to computer drop down menu
+ * @param {*} computer The computer object that will be added to the drop down menu 
+ */
+const addComputersToDropDownMenu = (computer) => {
     const computerElement = document.createElement("option");
     computerElement.value = computer.id;
     computerElement.appendChild(document.createTextNode(computer.title));
     computersElement.appendChild(computerElement);
 }
-
+/**
+ * When a change happens to the drop down menu this function handles this event and calls the render function so the new selected computer will be rendered
+ * @param {*} e the event 
+ */
 const handleComputerMenuChange = e => {
     const selectedComputer = computers[e.target.selectedIndex];
 
     renderComputer(selectedComputer);
 }
-
+/**
+ * Takes a computer as argument and fetches all features of the computer and return it a string
+ * @param {*} computer The computer you want to fetch features from
+ * @returns A String of all features that the computer have
+ */
 const getFeatures = (computer) =>{
     let features = "";
     for(let i = 0; i < computer.specs.length; i++){
@@ -53,7 +74,10 @@ const getFeatures = (computer) =>{
     }
     return features;
 }
-
+/**
+ * Renders features, image, title, description and price from the computer object to the Windows
+ * @param {*} computer The computer you want to render in the window
+ */
 const renderComputer = (computer) =>{
     computerFeatures.innerText = getFeatures(computer);
     computerImage.src = API + computer.image;
@@ -61,7 +85,10 @@ const renderComputer = (computer) =>{
     computerDescription.innerText = computer.description;
     computerPrice.innerText = computer.price + " KR";
 }
-
+/**
+ * Renders balance, loan and pay to the windows. If loan exists it will show the pay loan button and the outstanding loan.
+ * If no loan exists the button and div with loan amount will be hidden
+ */
 const renderBalance = () =>{
     balanceElement.innerText = balance + " Kr";
     payElement.innerText = pay + " Kr";
@@ -75,24 +102,29 @@ const renderBalance = () =>{
     }
 
 }
-
+/**
+ * Handle if the user want to loan money. First it checks if user already have a loan or not. If a loan is present the user cannot loan but i no loan is present the user can loan.
+ * Then the user inputs amount he or she wants and a check is done if it is an acceptable amount. If within acceptable amount loan is given, otherwise no loan is given. 
+ * When all is done it will render new loan or alert why loan wasn't given
+ */
 const handleGetLoan = () => {
-
-    const wantedLoan = prompt("How much do you want to loan?");
-
     if(loan > 0){
         alert("You already have a loan");
-        
-    }else if(parseInt(wantedLoan) <= 0 || parseInt(wantedLoan) > (balance * 2)){
-        alert("You can't loan that amount");
-        return;
     }else{
-    balance += parseInt(wantedLoan);
-    loan = parseInt(wantedLoan);
+        const wantedLoan = prompt("How much do you want to loan?");
+        
+        if(parseInt(wantedLoan) <= 0 || parseInt(wantedLoan) > (balance * 2)){
+            alert("You can't loan that amount");
+        }else{
+        balance += parseInt(wantedLoan);
+        loan = parseInt(wantedLoan);
+        }
     }
     renderBalance();
 }
-
+/**
+ *  Takes the pay amount and puts it into balance. If loan exists 10% is deducted to pay the loan. If loan is less than 10% only loan amount is deducted.
+ */
 const handleBank = () => {
     if(loan > 0){
         if((pay/100) * 10 > loan){
@@ -109,14 +141,17 @@ const handleBank = () => {
     
     renderBalance();
 }
-
+/**
+ * Adds 100 to the pay and render the new pay
+ */
 const handleWork = () => {
-    
     pay += 100;
-    
     renderBalance();
 }
-
+/**
+ * Reduces the loan by the pay and set pay to 0. If pay if more than loan, the whole loan gets paided and the remaining amount is put into balance. 
+ * If the loan gets paid, pay loan button and the loan amount gets hidden. 
+ */
 const handlePayLoan = () => {
     if(pay > loan){
         balance += (pay - loan);
@@ -128,7 +163,10 @@ const handlePayLoan = () => {
 
     renderBalance();
 }
-
+/**
+ * Tries to buy selected computer. If computer is in stock and user have enought money a computer will be bought. A computer will be removed from the stock and money removed from balance.
+ * If no computer is left in stock or user doesn't have enought money no computer will be bought and an error message will be shown.
+ */
 const handleBuyComputer = () => {
     const selectedComputer = computers[document.getElementById("computersElement").value-1];
     if(selectedComputer.stock <= 0){
@@ -144,6 +182,9 @@ const handleBuyComputer = () => {
     renderBalance();
 }
 
+/**
+ * Adds eventlisteners for all buttons and the drop down menu with computers
+ */
 computersElement.addEventListener("change", handleComputerMenuChange);
 getLoanButton.addEventListener("click", handleGetLoan);
 bankButton.addEventListener("click", handleBank);
